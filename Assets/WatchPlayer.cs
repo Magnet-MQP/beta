3ff49ab22edfd0f4325da68749048453e8a6e795
Joseph Henry Stadolnik IV <jhstadolnik@wpi.edu>
2021-03-16 01:55:40 -0400

    Level 3 Complete

---
+++ Assets/WatchPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WatchPlayer : MonoBehaviour
+{
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameManager.Instance.GetPlayer();
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            transform.rotation = Quaternion.LookRotation(transform.position-player.transform.position, Vector3.up);
+        }
+    }
+}
